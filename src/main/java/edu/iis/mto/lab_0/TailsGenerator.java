8daecc8e49c26353959432eb6e92fa905e39777c
Blus.Dawid <dawido312@gmail.com>
2019-03-06 18:25:04 +0100

    Funkcja przechodzi wszystkie testy

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -5,8 +5,15 @@ import java.util.List;
 
 public class TailsGenerator {
 
-    public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+    public List<String> tails(String value)
+    {
+
+        if(value==(null)) return new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        for(int i = 0;i<value.length();i++)
+            list.add(value.substring(i));
+        list.add("");
+        return list;
     }
 
 }
